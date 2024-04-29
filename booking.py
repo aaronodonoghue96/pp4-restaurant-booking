2317ae3e8e70d6ab1a0e18083749be10ad63355e
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2024-04-29 20:47:42 +0100

    Added booking class

---
+++ booking.py
@@ -0,0 +1,8 @@
+class Booking:
+
+    def __init__(self, bookingId, name, numPeople, date, time):
+        self.id = bookingId
+        self.name = name
+        self.numPeople = numPeople
+        self.date = date
+        self.time = time
