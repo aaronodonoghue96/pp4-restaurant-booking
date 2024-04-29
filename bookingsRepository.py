e1cbf64906dc72f6520887a81f01edbfe7f5c8dc
aaronodonoghue96 <aaronodonoghue2k11@gmail.com>
2024-04-29 20:52:47 +0100

    Add bookings repository

---
+++ bookingsRepository.py
@@ -0,0 +1,21 @@
+class BookingsRepository:
+    def __init__(self):
+        self.bookings = []
+
+    def placeBooking(self, bookings):
+        self.bookings.append(person)
+
+    def viewBookings(self):
+        return self.bookings
+
+    def update(self, old_booking, new_booking):
+        self.delete(old_booking.id)
+        self.add(new_booking)
+        raise Exception("No booking to update was found")
+
+    def delete(self, bookingId):
+        for b in self.bookings:
+            if b.id == bookingId:
+                self.bookings.remove(b)
+                return
+        raise Exception("No booking to delete was found")
